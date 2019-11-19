c239d16abb7fcc5a0128eed813d5a2384cf84d01
yuanyuan.ru <ruyuanyuan@1.com>
2019-11-19 10:17:21 +0800

    'webpack-dev-server'

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,10 +1,17 @@
 // webpack 是node 写出来的  node 的写法
 let path = require('path')
 module.exports = {
+  devServer:{
+    port:3000,
+    progress:true,
+    contentBase:path.resolve(__dirname,'../build'),
+    open:true,
+    compress:true
+  },
   mode:'development', // 模式  1. production 2.development
   entry:'./src/index.js' ,//入口
   output:{
     filename:'bundle.js', //打包后的文件名
-    path:path.resolve(__dirname,'build')
+    path:path.resolve(__dirname,'../build')
   }
 }
